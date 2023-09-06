1b32061830599311c2ca9700b4929edb15ba10dd
ksg0605 <ksg0605@gmail.com>
2023-09-06 20:21:54 +0900

    [add] Make Nav&Tab with storyboard

---
+++ Nav&Tab/Nav&Tab/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Nav&Tab
+//
+//  Created by 김선규 on 2023/09/06.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
