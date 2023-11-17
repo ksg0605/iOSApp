d400b0c069f9539f3f867a2a6c2712cbc475b908
ksg0605 <ksg0605@gmail.com>
2023-11-17 23:37:24 +0900

    [Add] Models

---
+++ Example-MusicApp/Example-MusicApp/Models/Music.swift
@@ -0,0 +1,42 @@
+//
+//  Music.swift
+//  Example-MusicApp
+//
+//  Created by 김선규 on 11/17/23.
+//
+
+import Foundation
+
+struct MusicData: Codable {
+    let resultCount: Int
+    let results: [Music]
+}
+
+struct Music: Codable {
+    let songName: String?
+    let artistName: String?
+    let albumName: String?
+    let previewUrl: String?
+    let imageUrl: String?
+    private let releaseDate: String?
+    
+    enum CodingKeys: String, CodingKey {
+        case songName = "trackName"
+        case artistName
+        case albumName = "collectionName"
+        case previewUrl
+        case imageUrl = "artworkUrl100"
+        case releaseDate
+    }
+    
+    var releaseDateString: String? {
+
+        guard let isoDate = ISO8601DateFormatter().date(from: releaseDate ?? "") else {
+            return ""
+        }
+        let myFormatter = DateFormatter()
+        myFormatter.dateFormat = "yyyy-MM-dd"
+        let dateString = myFormatter.string(from: isoDate)
+        return dateString
+    }
+}
